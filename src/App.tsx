2c62ba3bc7dc0b530d8613e756514dc9e86959c9
maxhartfield <maxhartfield@gmail.com>
2023-11-21 14:41:36 -0600

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <NavBar></NavBar>
-      <Routes>
-        <Route path="/portfolio/Home" element={<Home />}></Route>
-        <Route path="/portfolio/Projects" element={<ProjectPage />}></Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/Home" element={<Home />}></Route>
+          <Route path="/Projects" element={<ProjectPage />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
         <div className="navbar-nav ">
-          <Link className="nav-item nav-link active" to="/portfolio/Home">
+          <Link className="nav-item nav-link active" to="/Home">
             Home <span className="sr-only"></span>
           </Link>
-          <Link className="nav-item nav-link" to="/portfolio/Projects">
+          <Link className="nav-item nav-link" to="/Projects">
             Projects
           </Link>
         </div>
