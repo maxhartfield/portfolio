298383394ae0c611eb6d7f86496b7b4d841ed551
maxhartfield <maxhartfield@gmail.com>
2024-10-08 17:11:14 -0500

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,50 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-center fixed-top">
-        <div className="navbar-nav ">
-          <NavLink className="nav-item nav-link " to="/Home">
-            Home <span className="sr-only"></span>
-          </NavLink>
-          <NavLink className="nav-item nav-link " to="/Projects">
-            Projects
-          </NavLink>
-          <NavLink className="nav-item nav-link " to="/Internships">
-            Internships
-          </NavLink>
-          <NavLink className="nav-item nav-link " to="/Achievements">
-            Achievements
-          </NavLink>
-          <NavLink className="nav-item nav-link " to="/Resume">
-            Resume
-          </NavLink>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+          aria-controls="navbarNav"
+          aria-expanded={isOpen ? "true" : "false"}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}
+          id="navbarNav"
+        >
+          <div className="navbar-nav mx-auto">
+            {" "}
+            {/* Added mx-auto here */}
+            <NavLink className="nav-item nav-link" to="/Home">
+              Home <span className="sr-only"></span>
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/Projects">
+              Projects
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/Internships">
+              Internships
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/Achievements">
+              Achievements
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/Resume">
+              Resume
+            </NavLink>
+          </div>
         </div>
       </nav>
     </div>
